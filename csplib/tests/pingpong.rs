e540d045d53bc6a0f96bcb7d9e2e68b815b33cee
Akira Hayakawa <ruby.wktk@gmail.com>
2022-11-08 08:58:00 +0900

    Update pingpong test

--- csplib/tests/pingpong.rs
+++ csplib/tests/pingpong.rs
@@ -1,29 +1,45 @@
 use csplib::*;
 
+#[csplib::process]
+struct Ping {
+    #[input]
+    x: String,
+    #[output]
+    y: String,
+}
+async fn run_ping(inner: PingInner) -> Result<String> {
+    let s = "ping".to_owned();
+    inner.y.put(s)?;
+    tokio::task::yield_now().await;
+    let y = inner.x.reader().get().await?;
+    Ok(y)
+}
+#[csplib::process]
+struct Pong {
+    #[input]
+    x: String,
+    #[output]
+    y: String,
+}
+async fn run_pong(inner: PongInner) -> Result<()> {
+    let x = inner.x.reader().get().await?;
+    tokio::task::yield_now().await;
+    let s = format!("{}-pong", x);
+    inner.y.put(s).unwrap();
+    Ok(())
+}
+
 #[tokio::test]
 async fn pingpong() {
-    let (w1, ch1) = channel();
-    let (w2, ch2) = channel();
-    tokio::spawn({
-        let r1 = ch1.reader();
-        async move {
-            let x = r1.get().await.unwrap();
-            tokio::task::yield_now().await;
-            let s = format!("{}pong", x);
-            w2.put(s).unwrap();
-        }
-    });
-    let y = tokio::spawn({
-        let r2 = ch2.reader();
-        async move {
-            let x = "ping".to_owned();
-            w1.put(x).unwrap();
-            tokio::task::yield_now().await;
-            let y = r2.get().await.unwrap();
-            y
-        }
-    })
-    .await
-    .unwrap();
-    assert_eq!(y, "pingpong")
+    let (ping, ping_inner) = Ping::new();
+    let (pong, pong_inner) = Pong::new();
+
+    tokio::spawn(connect(ping.y.reader(), pong.x));
+    tokio::spawn(connect(pong.y.reader(), ping.x));
+
+    tokio::spawn(run_pong(pong_inner));
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+    let y = tokio::spawn(run_ping(ping_inner)).await.unwrap().unwrap();
+    assert_eq!(y, "ping-pong")
 }
